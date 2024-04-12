f5ec8d5dcd2d06be8a5a59829ba48be840a05ef5
Eduardo Salinas <eduardoluis93@gmail.com>
2024-04-12 13:46:41 +0100

    BUGGY: I'm printing the current level in the console as a DEBUG message. However, level 1 is never printed on the console.

--- Assets/GrabPickups.cs
+++ Assets/GrabPickups.cs
@@ -7,14 +7,84 @@ public class GrabPickups : MonoBehaviour {
 
 	private AudioSource pickupSoundSource;
 
+    /* Awake() function, which is like the init() function of a Love2D game.
+
+    I get it: why should I care that the current level number should not be eliminated while resetting the scene?
+    Because I don’t actually go to a new level each time that I pickup a purple coin: I just reset the current maze,
+    and a new random maze is generated. That is accomplished by resetting the Play scene each time that I pick up a
+    purple coin.
+
+	So, knowing that, I need to print onscreen a counter that says the current level.
+
+	To do that, I could simply add a counter. That counter would start at 1. That counter will be printed onscreen in
+	the Play scene. Then once I pick up a purple coin, that counter would go up by one. So, once I pick up a purple
+	coin and go to the next level, the counter would say “2” to indicate that I’m on level 2.
+
+	So, the counter should be “1 + number of purple coins picked up”. That way, in level 1, the counter would say “1”;
+	in level 2, the counter would say “1 + 1”, which, in this case, it’s “2”; and so on and so forth.
+
+	I could begin by just printing the current level as a debug message in the console. After I make it work, I would
+	later print it onscreen mid-level on the Play scene.
+
+	YUP: the question says that the text should ONLY be displayed on the Play scene, NOT on the Game over scene, nor on
+	the the Title scene.
+
+	This counter should DEFINITELY be detecting stuff every frame during the Play scene (being checked 60 times each
+	second). So, in which script should I declare the counter and print a debug message with its current value?
+
+	Should it be on the FPSCharacterController.cs file? Should I create a new MonoBehaviour script and attach it to the
+	FPSCharacterController game object? Where am I detecting if the player is colliding with the purple coins?
+	Whenever I detect if the player collides with the purple coins, I should add that the level counter should go up by
+	1.
+
+	I suspect that I should create the counter in the GrabPickups.cs file. Inside the OnControllerColliderHit()
+	function, I will update by 1 the counter. Then, on the Awake() function, which is pretty much the Unity equivalent
+	of the init() function in Love2D and Lua, I will first declare the counter, and initially give it a value of 1.
+
+    I forgot! I need to use static variables PRECISELY to prevent the variable from being reset while resetting the
+    scene! That’s what the homework assignment was warning me! So, I will add the keyword “static” to my variable, and
+    the currentLevel variable should stop from being reset back to 1!
+    */
+
+	// I will declare the counter which will keep track of the current level of the game (source: VS Code's Copilot)
+	private static int currentLevel = 1;
+
 	void Awake() {
 		pickupSoundSource = DontDestroy.instance.GetComponents<AudioSource>()[1];
+
+		// // DEBUG: This will show the current level on the console
+		// Debug.Log("Current Level: " + currentLevel);
 	}
 
+    /* This detects each time that the player collides with a purple coin.
+
+    I will make it so that, right before the new level is generated (the Play scene is reset), the counter that
+    keeps track of the current level will go up by one. That way, once I reach level 2, the ocunter will say "2"; "3"
+    on level "3", and so on and so forth.
+    */
 	void OnControllerColliderHit(ControllerColliderHit hit) {
 		if (hit.gameObject.tag == "Pickup") {
 			pickupSoundSource.Play();
+
+			// This will increase the counter by 1 to indicate that you've reached the next level (the next maze)
+			currentLevel += 1;
+
+            // DEBUG: This will show the current level on the console
+			Debug.Log("Current Level: " + currentLevel);
+
+			// This will reset the maze to give you the illusion that you're going to a new level (the next level)
 			SceneManager.LoadScene("Play");
 		}
 	}
+
+	/* I have an idea! I will add an Update() function on the GrabPickups.cs file. In it, I will print the current value of the currentLevel 
+	counter. This will be printed 60 times every second. Then, I will eliminate the debug message from the OnColliderHit() function that shows 
+	the current level. That way, the correct current level will always be printed on the console. Sure, the level will be printed 60 times every 
+	second, but, at least it will be always the correct level.
+	*/
+	// void Update() {
+
+	// 	// DEBUG: This will show the current level on the console 60 times every second
+	// 	Debug.Log("Current Level: " + currentLevel);
+	// }
 }
